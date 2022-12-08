675b7134864bbbcc17071d4fa6c8011436bf501c
Andreas Krath <andreas.krath+github@gmail.com>
2022-12-08 23:51:44 +0100

    yes

--- rust/pokechat/src/main.rs
+++ rust/pokechat/src/main.rs
@@ -1,38 +1,20 @@
 use std::io::stdin;
 
 fn main() {
-    let (mut letter_string, mut id_string) = (String::new(), String::new());
+    let (mut letter_string, mut number_string) = (String::new(), String::new());
     stdin()
         .read_line(&mut letter_string)
         .expect("failed to read letter_string");
     stdin()
-        .read_line(&mut id_string)
-        .expect("failed to read letter_string");
+        .read_line(&mut number_string)
+        .expect("failed to read number_string");
 
-    // loop over id string and construct encoding string
     let mut output_string: String = String::new();
-
-    for index in 0..id_string.chars().count() / 3 {
-        let mut temp_str: String = String::new();
-        for nested_index in 0..=2 {
-            temp_str.push(
-                id_string
-                    .chars()
-                    .nth((index * 3) + nested_index)
-                    .expect("failed to push sub string to temp_str"),
-            )
-        }
-
-        let current_id: usize = temp_str
-            .parse()
-            .expect("failed to parse temp_str int current_id");
-        println!("current_id: {}", current_id);
-
-        let temp_char: char = letter_string.chars().nth(current_id - 1).unwrap();
-
-        output_string.push(temp_char);
+    let number_string: Vec<char> = number_string.trim().chars().collect();
+    for id in number_string.chunks(3) {
+        let str: String = id.iter().collect();
+        let num: usize = str.trim().parse().unwrap();
+        output_string.push(letter_string.chars().nth(num - 1).unwrap());
     }
-
-    // print output
-    println!("{}", output_string.trim());
+    println!("{}", output_string);
 }
---
+++ rust/pokechat/target/debug/deps/pokechat-d4c0323ad4c010b4.d
@@ -0,0 +1,7 @@
+/home/krath/Documents/github/Kattis/rust/pokechat/target/debug/deps/pokechat-d4c0323ad4c010b4.rmeta: src/main.rs
+
+/home/krath/Documents/github/Kattis/rust/pokechat/target/debug/deps/pokechat-d4c0323ad4c010b4.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=-W__CLIPPY_HACKERY__clippy::pedantic__CLIPPY_HACKERY__
