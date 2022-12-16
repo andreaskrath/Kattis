822f8d8b973d883972263485bd61fe59ef791947
Andreas Krath <krath@pop-os.localdomain>
2022-12-16 23:12:29 +0100

    adding new linux host

--- rust/which_is_greater/src/main.rs
+++ rust/which_is_greater/src/main.rs
@@ -7,13 +7,13 @@ fn main() {
     let mut s: String = String::new();
     io::stdin().read_line(&mut s).expect("could not read");
 
-    let v: Vec<&str> = s.split(" ").collect();
+    let v: Vec<&str> = s.split_whitespace().collect();
     let a: i32 = v[0].trim().parse().unwrap();
     let b: i32 = v[1].trim().parse().unwrap();
 
     if a <= b {
         println!("0");
     } else {
-        println!("1")
+        println!("1");
     }
 }
