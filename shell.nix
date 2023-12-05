2cc5b700285163683ae879b3c76fe74feb590f37
Andreas Krath <andreas.krath+github@gmail.com>
2023-12-05 16:06:56 +0100

    dev: format shell.nix

--- shell.nix
+++ shell.nix
@@ -3,12 +3,14 @@ let
   rust = pkgs.rust-bin.stable."1.66.1".default.override {
     extensions = [ "rust-src" ];
   };
-  go_pin = import (pkgs.fetchFromGitHub{
-		owner = "nixos";
-		repo = "nixpkgs";
-		rev = "932fc16b263f26803d3960e4400bc13dde84a972";
-		sha256 = "sha256-f5XNR9i7ImhGHGys/0SpGX9OD2SFqWfNKlKtdn7c3Vc=";
-	}) {}; 
+  go_pin = import
+    (pkgs.fetchFromGitHub {
+      owner = "nixos";
+      repo = "nixpkgs";
+      rev = "932fc16b263f26803d3960e4400bc13dde84a972";
+      sha256 = "sha256-f5XNR9i7ImhGHGys/0SpGX9OD2SFqWfNKlKtdn7c3Vc=";
+    })
+    { };
 in
 pkgs.mkShell {
   nativeBuildInputs = [
