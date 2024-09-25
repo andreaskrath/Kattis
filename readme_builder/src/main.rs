0c045612e7b92828b76d1442d35331d4f5bc63ea
Andreas Krath <andreas.krath+github@gmail.com>
2024-09-25 19:36:39 +0200

    dev: rewrite some aspects of readme_builder

--- readme_builder/src/main.rs
+++ readme_builder/src/main.rs
@@ -1,26 +1,20 @@
 use glob::glob;
-use solution::Solution;
 use std::{
+    fmt::Display,
     fs::{self, OpenOptions},
     io::Write,
 };
 
-mod solution;
-
 fn main() {
-    let start_file_lines = fs::read_to_string("../README.md")
-        .unwrap()
-        .split('\n')
-        .count();
-    let mut rust_solutions = read_solutions("../rust/*/src/main.rs", "Rust");
-    let mut go_solutions = read_solutions("../go/*/main.go", "Go");
+    let start_file_lines = fs::read_to_string("../README.md").unwrap().lines().count();
+    let mut rust_solutions = read_solutions("../rust/*/src/main.rs", Language::Rust);
+    let mut go_solutions = read_solutions("../go/*/main.go", Language::Go);
+    let mut haskell_solutions = read_solutions("../haskell/*/main.hs", Language::Haskell);
     go_solutions.append(&mut rust_solutions);
+    go_solutions.append(&mut haskell_solutions);
     go_solutions.sort_unstable_by_key(|s| s.name.clone());
     build_table(go_solutions);
-    let end_files_lines = fs::read_to_string("../README.md")
-        .unwrap()
-        .split('\n')
-        .count();
+    let end_files_lines = fs::read_to_string("../README.md").unwrap().lines().count();
 
     println!(
         "{} lines were added to the README.md.",
@@ -36,8 +30,9 @@ fn build_table(solutions: Vec<Solution>) {
         .read(true)
         .write(true)
         .create(true)
+        .truncate(true)
         .open("../README.md")
-        .unwrap();
+        .expect("could not open README");
 
     _ = file.write(table_header.as_bytes()).unwrap();
     _ = file.write(table_orientation.as_bytes()).unwrap();
@@ -46,28 +41,82 @@ fn build_table(solutions: Vec<Solution>) {
     }
 }
 
-fn read_solutions(file_pattern: &str, prog_lang: &str) -> Vec<Solution> {
+fn read_solutions(file_pattern: &str, language: Language) -> Vec<Solution> {
     let mut solutions: Vec<Solution> = Vec::new();
 
     for path in glob(file_pattern).unwrap() {
         let path = path.unwrap().into_os_string().into_string().unwrap();
         let file = fs::read_to_string(&path).unwrap();
-        let lines: Vec<&str> = file.split('\n').collect();
+        let lines: Vec<&str> = file.lines().collect();
 
-        if !lines[0].starts_with("// ") || !lines[1].starts_with("// https") {
+        if !lines[0].starts_with(language.comment())
+            || !lines[1].starts_with(&format!("{} https", language.comment()))
+        {
             panic!("something is wrong with meta in {path}");
         }
 
-        let name = lines[0].trim_start_matches("//").trim();
-        let link = lines[1].trim_start_matches("//").trim();
+        let name = lines[0].trim_start_matches(language.comment()).trim();
+        let link = lines[1].trim_start_matches(language.comment()).trim();
 
         solutions.push(Solution::new(
             name.to_string(),
             link.to_string(),
-            String::from(prog_lang),
+            language.to_string(),
             path.trim_start_matches("../").trim().to_string(),
         ));
     }
 
     solutions
+}
+
+struct Solution {
+    pub name: String,
+    link: String,
+    language: String,
+    path: String,
+}
+
+impl Solution {
+    pub fn new(name: String, link: String, language: String, path: String) -> Self {
+        Self {
+            name,
+            link,
+            language,
+            path,
+        }
+    }
+
+    pub fn to_table_string(&self) -> String {
+        format!(
+            "|[{}]({})|[{}]({})|\n",
+            self.name, self.link, self.language, self.path
+        )
+    }
+}
+
+enum Language {
+    Rust,
+    Go,
+    Haskell,
+}
+
+impl Language {
+    fn comment(&self) -> &str {
+        match self {
+            Language::Rust | Language::Go => "//",
+            Language::Haskell => "--",
+        }
+    }
+}
+
+impl Display for Language {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            Language::Rust => "Rust",
+            Language::Go => "Go",
+            Language::Haskell => "Haskell",
+        };
+
+        write!(f, "{}", s)
+    }
 }
